106e21b273fb592ec9484091fc9568a533fc9e82
Kerbs <npdannug16@gmail.com>
2020-06-27 20:28:10 +0800

    Dev tool initial commit

---
+++ lib/dannug_view_tool.rb
@@ -0,0 +1,7 @@
+require "dannug_view_tool/version"
+require "dannug_view_tool/renderer"
+
+module DannugViewTool
+  class Error < StandardError; end
+  # Your code goes here...
+end
---
+++ lib/dannug_view_tool/version.rb
@@ -0,0 +1,3 @@
+module DannugViewTool
+  VERSION = "0.1.0"
+end
